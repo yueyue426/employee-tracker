091372c992e7be56b1e97e65de4316668cb2b32d
Chloe Jiang <yueyue426@berkeley.edu>
2024-07-07 10:43:37 -0700

    update schema file

--- db/schema.sql
+++ db/schema.sql
@@ -6,24 +6,24 @@ CREATE DATABASE company;
 -- Create a department table
 CREATE TABLE department (
     id INTEGER PRIMARY KEY,
-    name VARCHAR(30)
+    name VARCHAR(30) UNIQUE NOT NULL
 );
 
 -- Create a role table
 CREATE TABLE role (
     id INTEGER PRIMARY KEY,
-    title VARCHAR(30),
-    salary DECIMAL,
-    department_id INTEGER,
+    title VARCHAR(30) UNIQUE NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER NOT NULL,
     FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 -- Create a employee table
 CREATE TABLE employee (
     id INTEGER PRIMARY KEY,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    role_id INTEGER,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NOT NULL,
     manager_id INTEGER,
     FOREIGN KEY (role_id) REFERENCES role(id),
     FOREIGN KEY (manager_id) REFERENCES employee(id)
